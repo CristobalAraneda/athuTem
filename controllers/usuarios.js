f4963fc793be7b79791da0cbc65f365e3c6157ed
Cristobal Araneda <cristobalaraneda@MacBook-Air-de-Cristobal.local>
2022-12-07 10:48:57 -0300

    validacion de campos

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,23 +1,53 @@
+const { resp } = require('express')
+
 const Usuario = require('../models/usuario')
 
-const getUsuarios = (req, res) => {
+const getUsuarios = async(req, res) => {
+
+  const usuarios = await Usuario.find({}, 'nombre email role activo');
+
     res.json({
-      ok: true,
-      usuarios:[]
+      ok: true, 
+      usuarios
     });
   }
-const crearUsuarios = async(req, res) => {
+const crearUsuarios = async(req, resp) => {
 
      const{ email, password, nombre} = req.body;
 
-     const usuario = new Usuario( req.body );
+    
 
-     await usuario.save();
+     try {
+      const existeEmail = await Usuario.findOne( { email } );
 
-    res.json({
-      ok: true,
-      usuario
-    });
+      if ( existeEmail ) {
+
+        return resp.status(409).json({
+          ok: false,
+          msg: ' El correo ya se encuentra regristrado'
+        });
+        
+      }
+
+       const usuario = new Usuario( req.body );
+       await usuario.save();
+        
+       resp.status(201).json({
+         ok: true,
+         usuario
+       });
+      
+     } catch (error) {
+      console.log(error);
+      resp.status(500).json({
+        ok:false,
+        msg: process.env.ER500        
+      })
+      
+     }
+
+
+
   }
 
 
--- routes/usuario.js
+++ routes/usuario.js
@@ -1,11 +1,18 @@
 const{ Router} = require('express');
+const{ check } = require('express-validator');
+const{ validarCampos} = require('../middlewares/validar-campos')
 const{ getUsuarios,crearUsuarios } = require('../controllers/usuarios');
 
 const router = Router();
 
 
 router.get('/', getUsuarios );
-router.post('/', crearUsuarios );
+router.post('/',[
+     check('nombre','El nombre es requerido').not().isEmpty(),
+     check('password','El password es requerido').not().isEmpty(),
+     check('email','El email es requerido').isEmail(),
+     validarCampos
+], crearUsuarios );
   
 
 
